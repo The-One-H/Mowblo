6a7fb64d80a9c9ed2146e65476261a42b06b22c1
DarkMattrMaestro <94948678+DarkMattrMaestro@users.noreply.github.com>
2025-07-13 12:28:58 -0400

    Add base Legal sections

---
+++ app/(legal)/terms-of-service.tsx
@@ -0,0 +1,24 @@
+import { router } from 'expo-router';
+import { Text, TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const ToSPage = () => {
+  return (
+    <SafeAreaView className="flex-1 justify-center items-center bg-gray-100 p-5">
+        {/* Legal agreement */}
+        <Text className='text-3xl'>Terms of Service Agreement v0.0.-1</Text>
+        <Text className="text-center text-2xl font-bold text-gray-800 mb-5">Bla bla bla... You agree to sign away your life... Bla bla bla</Text>
+
+        {/* Placeholder page switching */}
+        <TouchableOpacity
+            onPress={() => {
+            router.back();
+            }}
+            className="absolute top-10 right-10 bg-blue-500 py-2 px-4 rounded-full">
+            <Text className="text-md font-JakartaBold text-white">Back</Text>
+        </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+export default ToSPage;
