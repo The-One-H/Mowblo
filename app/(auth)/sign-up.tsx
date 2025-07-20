351ac740697ceaa02986e677a313ffbcce3a6ab2
DarkMattrMaestro <94948678+DarkMattrMaestro@users.noreply.github.com>
2025-07-19 22:26:47 -0400

    Style and set up error management for email verification

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -2,9 +2,9 @@
 import { isClerkAPIResponseError, useSignUp } from '@clerk/clerk-expo'
 import { Link, useLocalSearchParams, useNavigation, useRouter } from 'expo-router'
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { Text, TextInput, TouchableOpacity, TouchableHighlight, StyleSheet, View, Button, Image, ScrollView } from 'react-native'
+import { Text, TextInput, TouchableOpacity, TouchableHighlight, StyleSheet, View, Button, Image, ScrollView, Keyboard } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
-import React from 'react'
+import React, { useRef } from 'react'
 import { useCallback, useEffect } from 'react'
 import * as WebBrowser from 'expo-web-browser'
 import * as AuthSession from 'expo-auth-session'
@@ -106,7 +106,7 @@ export default function SignUpScreen() {
   const [password, setPassword] = React.useState('')
   const [verifyPassword, setVerifyPassword] = React.useState('')
   const [pendingVerification, setPendingVerification] = React.useState(false)
-  const [code, setCode] = React.useState('')
+  const [code, setCode] = React.useState<string[]>(['', '', '', '', '', ''])
 
   const [errors, setErrors] = React.useState<ClerkAPIError[]>()
   const [showSigninError, setShowSigninError] = React.useState<String[]>([])
@@ -136,14 +136,30 @@ export default function SignUpScreen() {
     }
   }
 
+  const [showVerifyError, setShowVerifyError] = React.useState<number[]>([])
+  const [failedVerifyReason, setFailedVerifyReason] = React.useState('')
+
   // Handle submission of verification form
   const onVerifyPress = async () => {
     if (!isLoaded) return
 
+    // Check that the code was entered (redundant)
+    let missing: number[] = []
+    for (let i = 0; i < 6; i++) {
+      if (code[i].length != 1) {
+        missing.push(i);
+      }
+    }
+    if (missing.length > 0) {
+      setFailedVerifyReason(`Please enter your verification code.`)
+      setShowVerifyError(missing)
+      return;
+    }
+
     try {
       // Use the code the user provided to attempt verification
       const signUpAttempt = await signUp.attemptEmailAddressVerification({
-        code,
+        code: code.join(''),
       })
 
       // If verification was completed, set the session to active
@@ -160,21 +176,168 @@ export default function SignUpScreen() {
       // See https://clerk.com/docs/custom-flows/error-handling
       // for more info on error handling
       console.error(JSON.stringify(err, null, 2))
+
+      if (isClerkAPIResponseError(err)) {
+        const longMessages = err.errors.map((error) => {
+          switch (error.code) {
+            case "client_state_invalid":
+              router.replace({
+                pathname: '/(auth)/sign-up',
+                params: { navAnimation: 'fade' }
+              });
+              return error.longMessage?.substring(0,error.longMessage.length-1);
+            default:
+              return error.longMessage?.substring(0,error.longMessage.length-1);
+          }
+        })
+        setFailedVerifyReason(longMessages.join('; ') + '.')
+      }
+      setShowVerifyError([0, 1, 2, 3, 4, 5, 6])
+      setCode(['', '', '', '', '', ''])
     }
   }
 
+  const verifyRefs = useRef<any>([]);
+
+  useEffect(() => {
+    if (pendingVerification) {
+      verifyRefs.current[0].focus();
+    }
+  }, [])
+  
   if (pendingVerification) {
     return (
       <>
-        <Text>Verify your email</Text>
-        <TextInput
-          value={code}
-          placeholder="Enter your verification code"
-          onChangeText={(code) => setCode(code)}
-        />
-        <TouchableOpacity onPress={onVerifyPress}>
-          <Text>Verify</Text>
-        </TouchableOpacity>
+        <SafeAreaView className="flex-1 bg-gray-100">
+          <ScrollView
+            className='overflow-visible py-12'
+            alwaysBounceVertical={false}
+            automaticallyAdjustKeyboardInsets={true}
+            keyboardShouldPersistTaps={'never'}
+            showsVerticalScrollIndicator={false}
+          >
+            {/* Mowblo logo */}
+            <View className='px-12 flex content-center items-center'>
+              <Image
+                className=' w-1/2 h-[undefined] aspect-[303/71]'
+                resizeMode='contain'
+                source={require('../../assets/images/moblo-inline-transparent.png')}
+              />
+            </View>
+
+            {/* Login/out page switcher buffer (invisible and not interactable, used as a consistent spacer) */}
+            <View className='px-4 mt-3 mb-6 flex flex-row justify-end invisible'>
+              <TouchableOpacity
+                onPress={() => {
+                  router.replace({
+                    pathname: '/(auth)/sign-in',
+                    params: { navAnimation: 'slide_from_right' }
+                  });
+                }}
+                className='flex flex-row'
+              >
+                <Text className=' text-lg'>or Login</Text>
+                <Ionicons
+                  name={'chevron-forward-outline'}
+                  size={24}
+                  color={'black'}
+                ></Ionicons>
+              </TouchableOpacity>
+            </View>
+
+            {/* Content */}
+            <View className='px-12'>
+              <View className='flex content-center gap-6'>{/* Form content */}
+                {/* Titles */}
+                <View className='flex content-center items-center gap-0'>
+                  <Text className='text-xl font-bold center'>Veryify your email</Text>
+                  <Text>Enter the code we sent to your email</Text>
+                </View>
+
+                {/* Code verification section */}
+                <View className='flex content-center gap-3'>
+                  <View className='flex flex-row content-center justify-around gap-3'>
+                    {[1, 2, 3, 4, 5, 6].map((digit, i) => (
+                      <TextInput
+                        /* Password */
+                        className={' border text-lg font-bold rounded-lg m-0 p-4 focus:border-2 focus:m-[-1]'+(code[i].length > 0 ? ' bg-slate-200' : '')+((showVerifyError.includes(i)) ? ' border-red-300 focus:border-red-500' : ' border-slate-300 focus:border-slate-600')}
+                        autoCapitalize="none"
+                        autoComplete='one-time-code'
+                        enterKeyHint='done'
+                        autoCorrect={false}
+                        value={code[i]}
+                        key={digit}
+                        ref={(el) => {verifyRefs.current[i] = el}}
+                        placeholder="_"
+                        maxLength={1}
+                        placeholderTextColor={showVerifyError.includes(i) ? '#f87171' : '#64748b'}
+                        keyboardType='number-pad'
+
+                        /* Hide cursor and place it after the text */
+                        selection={{start: 1, end: 1}}
+                        caretHidden={true}
+
+                        onKeyPress={(e) => {
+                          setShowVerifyError(showVerifyError.filter((missing) => { return missing != i; }));
+                          if (e.nativeEvent.key == 'Backspace') {
+                            if (code[i] == '' && i > 0) {
+                              setCode(Object.assign([], code, {[i-1]: ''}));
+                              verifyRefs.current[i-1].focus();
+                            } else {
+                              setCode(Object.assign([], code, {[i]: ''}));
+                            }
+                          }
+                          else if (e.nativeEvent.key.length == 1) {
+                            setCode(Object.assign([], code, {[i]: e.nativeEvent.key}));
+                            if (i < code.length - 1) {
+                              verifyRefs.current[i+1].focus();
+                            } else {
+                              verifyRefs.current[i].focus(false);
+                              Keyboard.dismiss();
+                            }
+                          }
+                          // Any uncaught case is not a digit and can be safely ignored.
+                        }}
+                      />
+                    ))}
+                  </View>
+                  <TouchableOpacity
+                    className='bg-black disabled:bg-gray-400 rounded-lg m-0 p-3 items-center'
+                    disabled={code.join('').length != 6}
+                    onPress={onVerifyPress}
+                  >
+                    <Text className='color-white'>Verify</Text>
+                  </TouchableOpacity>
+                  {
+                    failedVerifyReason.length > 0 ?
+                      <Text className='color-red-500'>Verification failed: {failedVerifyReason}</Text> : null
+                  }
+                </View>
+              </View>
+            </View>
+          </ScrollView>
+        </SafeAreaView>
+        <SafeAreaView className="absolute">
+          {/* Login/out page switcher buffer (invisible and not interactable, used as a consistent spacer) */}
+          <View className='flex flex-row float-left items-center'>
+            <TouchableOpacity
+              onPress={() => {
+                router.replace({
+                  pathname: '/(auth)/sign-up',
+                  params: { navAnimation: 'slide_from_left' }
+                });
+              }}
+              className='px-4 py-4 flex flex-row'
+            >
+              <Ionicons
+                name={'chevron-back-outline'}
+                size={24}
+                color={'black'}
+              ></Ionicons>
+              {/* <Text className=' text-lg'>Back</Text> */}
+            </TouchableOpacity>
+          </View>
+        </SafeAreaView>
       </>
     )
   }
