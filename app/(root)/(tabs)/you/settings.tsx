bd3c7e25bfefd1f364486a2a84f219482df7e2d3
DarkMattrMaestro <94948678+DarkMattrMaestro@users.noreply.github.com>
2025-07-26 20:27:30 -0400

    Add local persistent storage

--- app/(root)/(tabs)/you/settings.tsx
+++ app/(root)/(tabs)/you/settings.tsx
@@ -1,11 +1,41 @@
 
-import { Text,TouchableOpacity,View } from 'react-native';
+import { PulldownButton } from '@/components/PulldownButton';
+import { useAsyncStorage } from '@/hooks/useAsyncStorage';
+import { AsyncStorageKey, getData, setData } from '@/utils/storage/AsyncStorage';
+import { ActivityIndicator, ScrollView, Text,TextInput,TouchableOpacity,View } from 'react-native';
 
 const SettingsScreen = () => {
-  return (
-      <View className="px-6 py-4 flex-row items-center justify-center">
+  const [darkMode, setDarkMode, loadingDarkMode] = useAsyncStorage(AsyncStorageKey.darkMode)
 
+  if (loadingDarkMode) {
+    return (
+      <View className='flex-1 items-center justify-center'>
+        <ActivityIndicator size="large" />
       </View>
-  );
+    )
+  }
+
+  return (
+      <ScrollView
+        className="flex-1"
+        automaticallyAdjustKeyboardInsets={true}
+        keyboardShouldPersistTaps={'never'}
+        showsVerticalScrollIndicator={false}
+      >
+        <View className="px-6 py-4 flex-col gap-3">
+          <View className='flex-1'>
+            <Text className='mx-3 my-1 font-semibold text-lg'>Dark Mode</Text>
+            <PulldownButton
+              valueMap={[
+                { key: 'Enabled', value: true },
+                { key: 'Disabled', value: false },
+              ]}
+              value={darkMode}
+              setter={(val) => setDarkMode(val)}
+            ></PulldownButton>
+          </View>
+        </View>
+      </ScrollView>
+    );
 };
 export default SettingsScreen;
---
+++ utils/storage/AsyncStorage.ts
@@ -0,0 +1,29 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+enum AsyncStorageKey {
+    darkMode = 'darkMode'
+}
+
+type AsyncStorageValue = {
+    [AsyncStorageKey.darkMode]: boolean
+};
+
+const setData = async <K extends AsyncStorageKey>(key: AsyncStorageKey, value: AsyncStorageValue[K]) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem(key, jsonValue);
+  } catch (e) {
+    // saving error
+  }
+};
+
+const getData = async <K extends AsyncStorageKey>(key: AsyncStorageKey): Promise<AsyncStorageValue[K] | undefined> => {
+  try {
+    const jsonValue = await AsyncStorage.getItem(key);
+    return jsonValue != null ? JSON.parse(jsonValue) : undefined;
+  } catch (e) {
+    // error reading value
+  }
+};
+
+export { setData, getData, AsyncStorageKey, AsyncStorageValue }
